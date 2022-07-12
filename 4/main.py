0b41ae5889a9935645be8780d1dc25a4decc2fdc
thor7777777 <thor7777777@mail.ru>
2022-07-12 10:04:26 +0300

    Доделки
    Внешние источники + ссылка на статью

--- 4/main.py
+++ 4/main.py
@@ -2,6 +2,7 @@ from lxml import html
 import requests
 from pymongo import MongoClient
 from pprint import pprint
+import re
 
 # БД
 client = MongoClient('127.0.0.1', 27017)
@@ -25,8 +26,16 @@ for item in items:
     name = item.xpath("./*[contains(@class, 'card-')]//*[contains(@class, '__title')]/text()")
     time = item.xpath(".//*[contains(@class, '__date')]/text()")
     href = item.xpath("./@href")
+    # Как оказалось внешние источники есть, Пример:
+    # https://moslenta.ru/news/city/vodopad-12-07-2022.htm/
+    if 'http' in href[0]:
+        # Если внешний источник:
+        source = re.search("(.*['ru'])", href)
+    else:
+        href = url + href[0]
+        source = url
 
-    new['Название'], new['Время'], new['Ссылка']= name, time, href
+    new['Название'], new['Время'], new['Ссылка'], new['Источник']= name, time, href, source
 
     news_db.insert_one(new) if news_db.find_one({'Ссылка': href}) == None else repeats_news.append(new)
 
